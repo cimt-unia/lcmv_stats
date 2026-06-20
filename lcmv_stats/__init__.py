1870331cd99f2b3161da528c6942f4fc4ecf6b00
ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>
2026-06-20 18:37:30 +0200

    Update __init__.py
    Signed-off-by: ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>

--- lcmv_stats/__init__.py
+++ lcmv_stats/__init__.py
@@ -8,13 +8,15 @@ from .epoching import extract_event_epochs, extract_continuous_epochs
 from .utils import get_subject_sfreq, map_subject_to_subj
 from .connectivity import extract_wpli_features, extract_gpdc_features
 from .statistics import run_edgewise_permutation, cohens_d_paired
-from .timefreq import compute_zscored_spectrogram, run_cluster_spectrogram, format_cluster_results_for_publication, extract_single_roi_epoch
+from .timefreq import compute_zscored_spectrogram, run_cluster_spectrogram, format_cluster_results_for_publication, extract_single_roi_epoch, run_roi_spectrogram_analysis
 from .visualization import plot_connectivity_matrix, validate_matrix_quality, plot_psd_rois
 from .reporting import plot_top_edges, generate_markdown_report, save_spectral_results, create_directed_effect_map
 
 from ._atlas import get_cimt_labels, get_motor_network_indices
 from .batch import prepare_group_comparison, prepare_connectivity_for_stats
 from . import batch
+
+
 
 
 
@@ -30,6 +32,7 @@ __all__ = [
     "run_edgewise_permutation",
     "cohens_d_paired",
     "extract_single_roi_epoch",
+    "run_roi_spectrogram_analysis",
     "compute_zscored_spectrogram",
     "run_cluster_spectrogram",
     "format_cluster_results_for_publication",
